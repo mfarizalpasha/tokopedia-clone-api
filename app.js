db7f2d55ddc2d29b519f44c8b3d1ccb3d0ed692b
mfarizalpasha <mfarizalpasha@gmail.com>
2023-08-19 12:19:55 +0700

    update

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./utils/db");
+const videoRoutes = require("./routes/videoRoutes");
+const productRoutes = require("./routes/productRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/videos", videoRoutes);
+app.use("/products", productRoutes);
+app.use("/comments", commentRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    API_list:
+      "https://gist.github.com/mfarizalpasha/46f488af9aa27988803ae3decf917c48",
+  });
+});
+
+connectDB().then(() => {
+  app.listen(5000, () => {
+    console.log("Server is running on http://localhost:5000");
+  });
+});
