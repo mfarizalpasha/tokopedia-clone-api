c9f50079e0458464456582d9a9ae2db21f8930e4
mfarizalpasha <mfarizalpasha@gmail.com>
2023-08-19 12:04:50 +0700

    update db

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const config = require("../config");
+// const config = require("../config");
 
 const uri =
   "mongodb+srv://mfarizalpasha:gomuno52431@cluster0.fuexwnx.mongodb.net/tokopedia-play-clone-app";
